79da5daafeb92b025f0111bb8e89c6f1236cd568
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-12-19 13:34:59 +0100

    styled the homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../styles/home.scss';
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
+      <div>
       <h1>Burger Shop!</h1>
       <p>Give yourself a tasty burger</p>
-      <button> <Link to="/menu">Explore Menu</Link></button>
-     
+      <button> <Link to="/menu" className='link'>Explore Menu</Link></button>
+      </div>
     </div>
   );
 }
