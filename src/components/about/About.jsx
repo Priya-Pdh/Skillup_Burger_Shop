133532f39e29d18c2f6a79d9b6e8e32e5c524795
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2023-12-19 15:42:14 +0100

    about us page task done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,1 +1,36 @@
 // Write all the code here
+import React from 'react';
+import '../../styles/home.scss';
+import founderPic from "../../assets/skj.jpg";
+import { FaSearch } from "react-icons/fa";
+ 
+const About = () => {
+  return (
+    <div className='about'>
+      <main>
+      <h1>About Us</h1>
+      <article>
+        <h4>Burger Shop</h4>
+      <p>This is Burger Shop. The place for most tasty burgers on the entire earth 🌎</p>
+      <p>Explore the various types of food and burgers. Click below to see the menu.</p>
+      <a href='/menu'><FaSearch /></a>
+      </article>
+
+      <div>
+        <h2>Founder</h2>
+        <article>
+            <div>
+        <img src={founderPic} alt="founder-picture" />
+       <h3>Nelson</h3>
+        </div>
+        <p>I am Nelson, the founder of Burger Shop. Affiliated to God Taste.</p>
+       
+        </article>
+
+    </div>
+      </main>
+    </div>
+  );
+}
+
+export default About;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,8 +7,9 @@ const Header = () => {
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
-          {/* <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/about">About</Link></li>
+          {/* <li><Link to="/contact">Contact</Link></li>
+         
           <li><Link to="/shopping-cart">Shopping Cart</Link></li>
           <li>
             <ul>
